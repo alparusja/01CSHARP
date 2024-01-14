b64d77a88f420eea947735dc9db7382f85c031cc
alparusja <alparusja@mail.ru>
2024-01-14 19:03:03 +0300

    add lesson03 Task1

---
+++ lesson03/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 1
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+double result = CalculateFormula(6, 2, 3, 4);
+Console.WriteLine(result);
