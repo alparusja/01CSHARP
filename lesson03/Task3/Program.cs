dda3a1153dad308149b6a0808374a7cf054434b2
alparusja <alparusja@mail.ru>
2024-01-14 19:47:57 +0300

    add lesson03 Task3

---
+++ lesson03/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3
+void ZeroIvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+void PtintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+int[] array = { 9, 1, 7, 8, 5 };
+ZeroIvenElements(array);
+PtintArray(array);
