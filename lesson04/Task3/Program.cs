77917e4abe5c490e779746cd7489824a07977b7b
alparusja <alparusja@mail.ru>
2024-02-06 21:17:24 +0300

    add lesson04_Task3

---
+++ lesson04/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string CetLetters(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = CetLetters(str);
+Console.WriteLine(result);
